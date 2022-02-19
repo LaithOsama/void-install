e6ca31661903b78d4fa66a1cf021eb3224f43ab1
laithosama <laith.osama.jassim@gmail.com>
2022-02-20 00:21:56 +0300

    update my script

--- void-install.sh
+++ void-install.sh
@@ -3,22 +3,22 @@
 #part1
 printf '\033c'
 
-echo -e "\e[32m  Preparing Disk and Filesystems for installation ..."
+echo -e "\e[32m  Preparing Disk and Filesystems for installation ...\e[0m"
 mkfs.ext4 /dev/sda1
 mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-echo -e "\e[32m  Doing the base installation ..."
+echo -e "\e[32m  Doing the base installation ...\e[0m"
 REPO=https://alpha.de.repo.voidlinux.org/current/musl
 ARCH=x86_64-musl
 mkdir -p /mnt/var/db/xbps/keys
 cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
-XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" base-minimal linux5.10 bash openssh dhcpcd kmod acpid neovim \
-e2fsprogs dracut ethtool iputils usbutils pciutils ncurses grub os-prober ntfs-3g
+XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" base-minimal linux4.19 bash openssh dhcpcd acpid neovim \
+e2fsprogs ethtool iputils usbutils pciutils ncurses grub os-prober ntfs-3g
 
-echo -e "\e[32m  Entering the Chroot ..."
+echo -e "\e[32m  Entering the Chroot ...\e[0m"
 mount --rbind /sys /mnt/sys && mount --make-rslave /mnt/sys
 mount --rbind /dev /mnt/dev && mount --make-rslave /mnt/dev
 mount --rbind /proc /mnt/proc && mount --make-rslave /mnt/proc
@@ -31,48 +31,41 @@ exit
 #part2
 printf '\033c'
 
-echo -e "\e[32m  Configuring fstab ..."
+echo -e "\e[32m  Configuring fstab ...\e[0m"
 cp /proc/mounts /etc/fstab
-blkid /dev/sda2 >> /etc/fstab
-blkid /dev/sda1 >> /etc/fstab
-blkid /dev/sda3 >> /etc/fstab
 nvim /etc/fstab
 echo "tmpfs    /tmp     tmpfs   defaults,nosuid,nodev   0 0" >> /etc/fstab
 
-echo -e "\e[32m  Setting up swapfile, localtime and hostname ... etc."
-dd if=/dev/zero of=/swapfile bs=1G count=8 status=progress
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo "/swapfile none swap defaults 0 0" >> /etc/fstab 
+echo -e "\e[32m  Setting up localtime and hostname ... etc.\e[0m"
 ln -sf /usr/share/zoneinfo/Asia/Baghdad /etc/localtime
-echo hostonly=yes >> /etc/dracut.conf
 echo "laptop" > /etc/hostname
 
-echo -e "\e[32m  Grub installation ..."
+echo -e "\e[32m  Grub installation ...\e[0m"
 grub-install /dev/sda
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+ech 'GRUB_CMDLINE_LINUX="root=/dev/sda2 rootfstype=ext4"' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo -e "\e[32m  Install Packages ..."
+echo -e "\e[32m  Install Packages ...\e[0m"
 xbps-install -Sy xorg-minimal gcc make pkg-config libXft-devel libX11-devel libXinerama-devel \
 hsetroot sxiv zathura zathura-pdf-mupdf stow maim xset xrandr xclip firefox git \
 mpv cmus cmus-opus cmus-flac newsboat unzip wget calcurse yt-dlp xdotool dosfstools \
-zsh transmission man-db pfetch fzf bc picom opendoas \
+zsh transmission mdocml pfetch fzf bc picom opendoas dejavu-fonts-ttf \
 htop alsa-utils void-repo-nonfree && xbps-install -Sy unrar
 
-echo -e "\e[32m  Install intel drivers ..."
-xbps-install -y xf86-video-intel mesa mesa-vaapi intel-ucode intel-video-accel linux-firmware-intel
-read -p "Do you need extra packages like libreoffice, kodi and some games ? [y/N] " answer
+echo -e "\e[32m  Install intel drivers ...\e[0m"
+xbps-install -y xf86-video-intel mesa-vaapi libva-intel-driver libva-utils intel-ucode
+read -p "Do you need extra packages like libreoffice, gimp and kodi ? [y/N] " answer
 if [[ $answer = y ]] ; then
-  xbps-install -y libreoffice-calc fractal kodi supertuxkart sauerbraten
+  xbps-install -y libreoffice-calc gimp fractal kodi
 fi
 rm -rf /var/cache/xbps/*
 
-echo -e "\e[32m  Setting up users, mouse speed, keyboard langs ... etc."
+echo -e "\e[32m  Setting up users, mouse speed, keyboard langs ... etc.\e[0m"
 passwd
 echo "permit nopass :wheel" >> /etc/doas.conf
-useradd -m -G wheel audio -s /bin/zsh laith
+useradd -m -G wheel -s /bin/zsh laith
+usermod -g audio laith
 passwd laith
 mkdir -pv /etc/X11/xorg.conf.d
 echo 'Section "InputClass"
@@ -90,19 +83,19 @@ echo 'Section "InputClass"
   	Option "XkbVariant" ",qwerty"
   	Option "XkbOptions" "grp:win_space_toggle"
 EndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf
-echo 'Section "Device"
-	Identifier "Intel Graphics"
-	Driver "modesetting"
-EndSection' >> /etc/X11/xorg.conf.d/20-intel.conf
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
 doas cat hosts >> /etc/hosts && rm hosts
 
-echo -e "\e[32m  Making sure that all installed packages are configured properly ..."
+echo -e "\e[32m  Making sure that all installed packages are configured properly ...\e[0m"
 xbps-reconfigure -fa
 
-echo -e "\e[32m  Enabling services ..."
+echo -e "\e[32m  Enabling and Disabling services ...\e[0m"
 ln -s /etc/sv/alsa /etc/runit/runsvdir/default/
 ln -s /etc/sv/dhcpcd /etc/runit/runsvdir/default/
+rm -rf /etc/sv/agetty-tty3
+rm -rf /etc/sv/agetty-tty4
+rm -rf /etc/sv/agetty-tty5
+rm -rf /etc/sv/sshd
 
 vi3_path=/home/laith/void-install3.sh
 sed '1,/^#part3$/d' void-install2.sh > $vi3_path
@@ -116,12 +109,12 @@ printf '\033c'
 cd $HOME
 rm -rf *.* .*
 
-echo -e "\e[32m  Downloading and managing dotfiles ..."
+echo -e "\e[32m  Downloading and managing dotfiles ...\e[0m"
 git clone https://github.com/LaithOsama/.dotfiles.git 
 cd .dotfiles && stow .
 cd ..
 
-echo -e "\e[32m  Install dwm, st, slstatus and dmenu (Suckless Tools) ..."
+echo -e "\e[32m  Install dwm, st, slstatus and dmenu (Suckless Tools) ...\e[0m"
 git clone --depth=1 https://github.com/LaithOsama/dwm.git ~/.local/src/dwm
 doas make -C ~/.local/src/dwm install
 git clone --depth=1 https://github.com/LaithOsama/st.git ~/.local/src/st
@@ -131,7 +124,7 @@ doas make -C ~/.local/src/dmenu install
 git clone --depth=1 https://github.com/LaithOsama/slstatus.git ~/.local/src/slstatus
 doas make -C ~/.local/src/slstatus install
 
-echo -e "\e[32m  We're almost done, don't forget to curse the neoliberal regimes and America :)"
+echo -e "\e[32m  We're almost done, don't forget to curse the neoliberal regimes and America :)\e[0m"
 doas git clone https://github.com/zdharma-continuum/fast-syntax-highlighting /usr/share/zsh/plugins/fast-syntax-highlighting
 mkdir -p ~/.cache/zsh ~/data ~/dl/git
 touch ~/.cache/zsh/history
